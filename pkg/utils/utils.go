3124b18b3c07e8bfe2887058b22930bcbcc0a78c
Techno Freak <freak12techno@gmail.com>
2023-01-25 18:37:06 +0300

    fix: fixed time formatting

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,5 +1,12 @@
 package utils
 
+import (
+	"fmt"
+	"math"
+	"strings"
+	"time"
+)
+
 func Filter[T any](slice []T, f func(T) bool) []T {
 	var n []T
 	for _, e := range slice {
@@ -23,4 +30,36 @@ func ResolveVote(value string) string {
 	}
 
 	return value
+}
+
+func FormatDuration(duration time.Duration) string {
+	days := int64(duration.Hours() / 24)
+	hours := int64(math.Mod(duration.Hours(), 24))
+	minutes := int64(math.Mod(duration.Minutes(), 60))
+	seconds := int64(math.Mod(duration.Seconds(), 60))
+
+	chunks := []struct {
+		singularName string
+		amount       int64
+	}{
+		{"day", days},
+		{"hour", hours},
+		{"minute", minutes},
+		{"second", seconds},
+	}
+
+	parts := []string{}
+
+	for _, chunk := range chunks {
+		switch chunk.amount {
+		case 0:
+			continue
+		case 1:
+			parts = append(parts, fmt.Sprintf("%d %s", chunk.amount, chunk.singularName))
+		default:
+			parts = append(parts, fmt.Sprintf("%d %ss", chunk.amount, chunk.singularName))
+		}
+	}
+
+	return strings.Join(parts, " ")
 }
