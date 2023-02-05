69c7be56584f564e53ff5e4508670c5756b3b60a
Techno Freak <freak12techno@gmail.com>
2023-02-06 01:49:24 +0300

    feat: load proposals and votes in parallel

--- pkg/state/generator/state_generator.go
+++ pkg/state/generator/state_generator.go
@@ -4,6 +4,8 @@ import (
 	configTypes "main/pkg/config/types"
 	statePackage "main/pkg/state"
 	"main/pkg/tendermint"
+	"main/pkg/types"
+	"sync"
 
 	"github.com/rs/zerolog"
 )
@@ -11,6 +13,7 @@ import (
 type StateGenerator struct {
 	Logger zerolog.Logger
 	Chains configTypes.Chains
+	Mutex  sync.Mutex
 }
 
 func NewStateGenerator(logger *zerolog.Logger, chains configTypes.Chains) *StateGenerator {
@@ -23,72 +26,109 @@ func NewStateGenerator(logger *zerolog.Logger, chains configTypes.Chains) *State
 func (g *StateGenerator) GetState(oldState statePackage.State) statePackage.State {
 	state := statePackage.NewState()
 
+	var wg sync.WaitGroup
+	wg.Add(len(g.Chains))
+
 	for _, chain := range g.Chains {
 		g.Logger.Info().Str("name", chain.Name).Msg("Processing a chain")
+		go func(c *configTypes.Chain) {
+			g.ProcessChain(c, state, oldState)
+			wg.Done()
+		}(chain)
+	}
 
-		rpc := tendermint.NewRPC(chain.LCDEndpoints, g.Logger)
+	wg.Wait()
+	return state
+}
 
-		proposals, err := rpc.GetAllProposals()
-		if err != nil {
-			g.Logger.Warn().Err(err).Msg("Error processing proposals")
-			state.SetChainProposalsError(chain, err)
-			continue
-		}
+func (g *StateGenerator) ProcessChain(
+	chain *configTypes.Chain,
+	state statePackage.State,
+	oldState statePackage.State,
+) {
+	rpc := tendermint.NewRPC(chain.LCDEndpoints, g.Logger)
 
-		g.Logger.Info().Int("len", len(proposals)).Msg("Got proposals")
+	proposals, err := rpc.GetAllProposals()
+	if err != nil {
+		g.Logger.Warn().Err(err).Msg("Error processing proposals")
+		g.Mutex.Lock()
+		state.SetChainProposalsError(chain, err)
+		g.Mutex.Unlock()
+		return
+	}
 
-		for _, proposal := range proposals {
+	g.Logger.Info().Int("len", len(proposals)).Msg("Got proposals")
+
+	var wg sync.WaitGroup
+
+	for _, proposal := range proposals {
+		g.Logger.Trace().
+			Str("name", chain.Name).
+			Str("proposal", proposal.ProposalID).
+			Msg("Processing a proposal")
+
+		for _, wallet := range chain.Wallets {
 			g.Logger.Trace().
 				Str("name", chain.Name).
 				Str("proposal", proposal.ProposalID).
-				Msg("Processing a proposal")
+				Str("wallet", wallet.Address).
+				Msg("Processing wallet vote")
+			wg.Add(1)
 
-			for _, wallet := range chain.Wallets {
-				g.Logger.Trace().
-					Str("name", chain.Name).
-					Str("proposal", proposal.ProposalID).
-					Str("wallet", wallet.Address).
-					Msg("Processing wallet vote")
-
-				oldVote, _, found := oldState.GetVoteAndProposal(chain.Name, proposal.ProposalID, wallet.Address)
-				voteResponse, err := rpc.GetVote(proposal.ProposalID, wallet.Address)
-
-				if found && oldVote.HasVoted() && voteResponse.Vote == nil {
-					g.Logger.Trace().
-						Str("chain", chain.Name).
-						Str("proposal", proposal.ProposalID).
-						Str("wallet", wallet.Address).
-						Msg("Wallet has voted and there's no vote in the new state - using old vote")
-
-					state.SetVote(
-						chain,
-						proposal,
-						wallet,
-						oldVote,
-					)
-
-					continue
-				}
-
-				proposalVote := statePackage.ProposalVote{
-					Wallet: wallet,
-				}
-
-				if err != nil {
-					proposalVote.Error = err.Error()
-				} else {
-					proposalVote.Vote = voteResponse.Vote
-				}
-
-				state.SetVote(
-					chain,
-					proposal,
-					wallet,
-					proposalVote,
-				)
-			}
+			go func(p types.Proposal, w *configTypes.Wallet) {
+				g.ProcessProposalAndWallet(chain, p, rpc, w, state, oldState)
+				wg.Done()
+			}(proposal, wallet)
 		}
 	}
 
-	return state
+	wg.Wait()
+}
+
+func (g *StateGenerator) ProcessProposalAndWallet(
+	chain *configTypes.Chain,
+	proposal types.Proposal,
+	rpc *tendermint.RPC,
+	wallet *configTypes.Wallet,
+	state statePackage.State,
+	oldState statePackage.State,
+) {
+	oldVote, _, found := oldState.GetVoteAndProposal(chain.Name, proposal.ProposalID, wallet.Address)
+	voteResponse, err := rpc.GetVote(proposal.ProposalID, wallet.Address)
+
+	if found && oldVote.HasVoted() && voteResponse.Vote == nil {
+		g.Logger.Trace().
+			Str("chain", chain.Name).
+			Str("proposal", proposal.ProposalID).
+			Str("wallet", wallet.Address).
+			Msg("Wallet has voted and there's no vote in the new state - using old vote")
+
+		g.Mutex.Lock()
+		state.SetVote(
+			chain,
+			proposal,
+			wallet,
+			oldVote,
+		)
+		g.Mutex.Unlock()
+	}
+
+	proposalVote := statePackage.ProposalVote{
+		Wallet: wallet,
+	}
+
+	if err != nil {
+		proposalVote.Error = err.Error()
+	} else {
+		proposalVote.Vote = voteResponse.Vote
+	}
+
+	g.Mutex.Lock()
+	state.SetVote(
+		chain,
+		proposal,
+		wallet,
+		proposalVote,
+	)
+	g.Mutex.Unlock()
 }
