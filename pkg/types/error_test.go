bfa08bc8dab95b017660ca7a7b75dd7d25ec7eda
Techno Freak <freak12techno@gmail.com>
2023-12-07 17:33:47 +0300

    chore: fix linting

--- pkg/types/error_test.go
+++ pkg/types/error_test.go
@@ -2,8 +2,9 @@ package types
 
 import (
 	"errors"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestQueryErrorSerializeWithQueryError(t *testing.T) {
