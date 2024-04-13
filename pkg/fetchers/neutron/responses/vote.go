3eaccf92ff7ea9646a664dc3fd65556929870658
Sergey <83376337+freak12techno@users.noreply.github.com>
2024-04-14 02:49:33 +0300

    feat: add emojis to votes (#69)
    * feat: add emojis to votes
    
    * chore: fixed linting

--- pkg/fetchers/neutron/responses/vote.go
+++ pkg/fetchers/neutron/responses/vote.go
@@ -11,9 +11,9 @@ type Vote struct {
 
 func (v Vote) GetOption() string {
 	options := map[string]string{
-		"yes":     "Yes",
-		"no":      "No",
-		"abstain": "Abstain",
+		"yes":     "👌Yes",
+		"no":      "🚫No",
+		"abstain": "🤷Abstain",
 	}
 
 	if option, ok := options[v.Vote]; ok {
