3db1a9f8e2c3ef7d2fd36c004b841a2cd925d7f8
Sergey <freak12techno@gmail.com>
2024-05-03 08:22:03 +0300

    chore: add more tests

--- pkg/fs/test_fs.go
+++ pkg/fs/test_fs.go
@@ -1,20 +1,37 @@
 package fs
 
 import (
+	"errors"
 	"main/assets"
 	"os"
 )
 
-type TestFS struct{}
+type TestFS struct {
+	WithWriteError  bool
+	WithCreateError bool
+
+	WithFileWriteError bool
+	WithFileCloseError bool
+}
 
 type TestFile struct {
+	WithFileWriteError bool
+	WithFileCloseError bool
 }
 
 func (f *TestFile) Write(p []byte) (int, error) {
+	if f.WithFileWriteError {
+		return 0, errors.New("stub error")
+	}
+
 	return 0, nil
 }
 
 func (f *TestFile) Close() error {
+	if f.WithFileCloseError {
+		return errors.New("stub error")
+	}
+
 	return nil
 }
 
@@ -23,9 +40,20 @@ func (fs *TestFS) ReadFile(name string) ([]byte, error) {
 }
 
 func (fs *TestFS) WriteFile(name string, data []byte, perms os.FileMode) error {
+	if fs.WithWriteError {
+		return errors.New("stub error")
+	}
+
 	return nil
 }
 
 func (fs *TestFS) Create(path string) (File, error) {
-	return &TestFile{}, nil // go
+	if fs.WithCreateError {
+		return nil, errors.New("stub error")
+	}
+
+	return &TestFile{
+		WithFileWriteError: fs.WithFileWriteError,
+		WithFileCloseError: fs.WithFileCloseError,
+	}, nil
 }
