d50a2c304d96af46ab588b25526c7e49f7895c85
Sergey <83376337+freak12techno@users.noreply.github.com>
2024-05-03 15:50:09 +0300

    chore: more tests (#80)
    * chore: add load_config tests
    
    * chore: add more tests
    
    * chore: fixed linting
    
    * chore: committing files

--- pkg/fs/test_fs.go
+++ pkg/fs/test_fs.go
@@ -1,30 +1,59 @@
 package fs
 
 import (
+	"errors"
+	"main/assets"
 	"os"
 )
 
-type TestFS struct{}
+type TestFS struct {
+	WithWriteError  bool
+	WithCreateError bool
+
+	WithFileWriteError bool
+	WithFileCloseError bool
+}
 
 type TestFile struct {
+	WithFileWriteError bool
+	WithFileCloseError bool
 }
 
 func (f *TestFile) Write(p []byte) (int, error) {
+	if f.WithFileWriteError {
+		return 0, errors.New("stub error")
+	}
+
 	return 0, nil
 }
 
 func (f *TestFile) Close() error {
+	if f.WithFileCloseError {
+		return errors.New("stub error")
+	}
+
 	return nil
 }
 
 func (fs *TestFS) ReadFile(name string) ([]byte, error) {
-	return []byte{}, nil
+	return assets.EmbedFS.ReadFile(name)
 }
 
 func (fs *TestFS) WriteFile(name string, data []byte, perms os.FileMode) error {
+	if fs.WithWriteError {
+		return errors.New("stub error")
+	}
+
 	return nil
 }
 
 func (fs *TestFS) Create(path string) (File, error) {
-	return &TestFile{}, nil // go
+	if fs.WithCreateError {
+		return nil, errors.New("stub error")
+	}
+
+	return &TestFile{
+		WithFileWriteError: fs.WithFileWriteError,
+		WithFileCloseError: fs.WithFileCloseError,
+	}, nil
 }
---
+++ pkg/mutes/manager_test.go
@@ -0,0 +1,132 @@
+package mutesmanager
+
+import (
+	"main/pkg/fs"
+	"main/pkg/logger"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMuteManagerLoadWithoutPath(t *testing.T) {
+	t.Parallel()
+
+	log := logger.GetNopLogger()
+	filesystem := &fs.TestFS{}
+
+	manager := NewMutesManager("", filesystem, log)
+	manager.Load()
+
+	assert.Empty(t, manager.Mutes.Mutes)
+}
+
+func TestMuteManagerLoadNotExistingPath(t *testing.T) {
+	t.Parallel()
+
+	log := logger.GetNopLogger()
+	filesystem := &fs.TestFS{}
+
+	manager := NewMutesManager("not-existing.json", filesystem, log)
+	manager.Load()
+
+	assert.Empty(t, manager.Mutes.Mutes)
+}
+
+func TestMuteManagerLoadInvalidJson(t *testing.T) {
+	t.Parallel()
+
+	log := logger.GetNopLogger()
+	filesystem := &fs.TestFS{}
+
+	manager := NewMutesManager("invalid-json.json", filesystem, log)
+	manager.Load()
+
+	assert.Empty(t, manager.Mutes.Mutes)
+}
+
+func TestMuteManagerLoadValidJson(t *testing.T) {
+	t.Parallel()
+
+	log := logger.GetNopLogger()
+	filesystem := &fs.TestFS{}
+
+	manager := NewMutesManager("valid-mutes.json", filesystem, log)
+	manager.Load()
+
+	assert.NotEmpty(t, manager.Mutes.Mutes)
+}
+
+func TestMuteManagerSaveWithoutPath(t *testing.T) {
+	t.Parallel()
+
+	log := logger.GetNopLogger()
+	filesystem := &fs.TestFS{}
+
+	manager := NewMutesManager("", filesystem, log)
+	manager.Load()
+	manager.Save()
+
+	assert.Empty(t, manager.Mutes.Mutes)
+}
+
+func TestMuteManagerSaveWithError(t *testing.T) {
+	t.Parallel()
+
+	log := logger.GetNopLogger()
+	filesystem := &fs.TestFS{WithWriteError: true}
+
+	manager := NewMutesManager("out.json", filesystem, log)
+	manager.Load()
+	manager.Save()
+
+	assert.Empty(t, manager.Mutes.Mutes)
+}
+
+func TestMuteManagerSaveWithoutError(t *testing.T) {
+	t.Parallel()
+
+	log := logger.GetNopLogger()
+	filesystem := &fs.TestFS{}
+
+	manager := NewMutesManager("out.json", filesystem, log)
+	manager.Load()
+	manager.Save()
+
+	assert.Empty(t, manager.Mutes.Mutes)
+}
+
+func TestMuteManagerAddMuteIsMuted(t *testing.T) {
+	t.Parallel()
+
+	log := logger.GetNopLogger()
+	filesystem := &fs.TestFS{}
+
+	manager := NewMutesManager("out.json", filesystem, log)
+	manager.Load()
+
+	manager.AddMute(&Mute{
+		Chain:   "chain",
+		Expires: time.Now().Add(time.Hour),
+	})
+
+	assert.True(t, manager.IsMuted("chain", "proposal"))
+	assert.False(t, manager.IsMuted("chain2", "proposal"))
+}
+
+func TestMuteManagerIsMutedNoPath(t *testing.T) {
+	t.Parallel()
+
+	log := logger.GetNopLogger()
+	filesystem := &fs.TestFS{}
+
+	manager := NewMutesManager("", filesystem, log)
+	manager.Load()
+
+	manager.AddMute(&Mute{
+		Chain:   "chain",
+		Expires: time.Now().Add(time.Hour),
+	})
+
+	assert.False(t, manager.IsMuted("chain", "proposal"))
+}
