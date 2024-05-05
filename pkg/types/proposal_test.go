7f7b25198ac6e986b3d50145ee77a6d3f3cf2a99
Sergey <freak12techno@gmail.com>
2024-05-05 11:03:50 +0300

    chore: add more tests for proposal

--- pkg/types/proposal_test.go
+++ pkg/types/proposal_test.go
@@ -16,3 +16,22 @@ func TestProposalGetVotingTime(t *testing.T) {
 
 	assert.Equal(t, "1 hour 1 minute", proposal.GetTimeLeft(), "Wrong value!")
 }
+
+func TestProposalInVoting(t *testing.T) {
+	t.Parallel()
+
+	assert.True(t, Proposal{Status: ProposalStatusVoting}.IsInVoting())
+	assert.False(t, Proposal{Status: ProposalStatusPassed}.IsInVoting())
+}
+
+func TestProposalStatusSerialize(t *testing.T) {
+	t.Parallel()
+
+	assert.Equal(t, "🗳️Voting", Proposal{Status: ProposalStatusVoting}.SerializeStatus())
+	assert.Equal(t, "💸Deposit", Proposal{Status: ProposalStatusDeposit}.SerializeStatus())
+	assert.Equal(t, "🙌 Passed", Proposal{Status: ProposalStatusPassed}.SerializeStatus())
+	assert.Equal(t, "🙅‍Rejected", Proposal{Status: ProposalStatusRejected}.SerializeStatus())
+	assert.Equal(t, "🤦‍Failed", Proposal{Status: ProposalStatusFailed}.SerializeStatus())
+	assert.Equal(t, "test", Proposal{Status: ProposalStatus("test")}.SerializeStatus())
+
+}
