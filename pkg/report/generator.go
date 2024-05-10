d99d3b9cc6d745bdf355d46f6aa7c441df4f42d7
Sergey <freak12techno@gmail.com>
2024-05-10 21:02:10 +0300

    fix: fixed finished proposals notifications

--- pkg/report/generator.go
+++ pkg/report/generator.go
@@ -46,6 +46,42 @@ func (g *Generator) GenerateReport(oldState, newState state.State) reporters.Rep
 		}
 
 		for proposalID, proposalVotes := range chainInfo.ProposalVotes {
+			oldProposal, oldProposalFound := oldState.GetProposal(chainName, proposalID)
+			newProposal := proposalVotes.Proposal
+
+			if oldProposalFound {
+				// There can be the following cases:
+				// 1) old proposal not found - this is a new proposal, no custom logic needed.
+				// 2) old proposal is found, it's not in voting and the current one is not in voting
+				// (like deposit -> deposit) - we don't need to report it at all.
+				// 3) old proposal is found, it's not in voting but the current one is in voting
+				// (like deposit -> deposit) - no custom logic needed.
+				// 4) old proposal is found, it's in voting and the current one is in voting
+				// (like voting -> voting) - no custom logic needed.
+				// 5) old proposal is found, it's in voting and the current one is not in voting
+				// (like voting -> passed) -> send a new event that the voting has finished.
+
+				// case 1, 3 and 4 is handled outside of this if case
+				if oldProposal.IsInVoting() && !newProposal.IsInVoting() { // case 5
+					g.Logger.Debug().
+						Str("chain", chainName).
+						Str("proposal", proposalID).
+						Msg("Previously proposal was in voting, but it's not now - sending an alert")
+
+					entries = append(entries, events.FinishedVotingEvent{
+						Chain:    g.Chains.FindByName(chainName),
+						Proposal: newProposal,
+					})
+					continue
+				} else if !oldProposal.IsInVoting() && !newProposal.IsInVoting() { // case 2
+					g.Logger.Debug().
+						Str("chain", chainName).
+						Str("proposal", proposalID).
+						Msg("Previously proposal was and is not in voting period - ignoring.")
+					continue
+				}
+			}
+
 			for wallet := range proposalVotes.Votes {
 				g.Logger.Trace().
 					Str("name", chainName).
@@ -55,44 +91,6 @@ func (g *Generator) GenerateReport(oldState, newState state.State) reporters.Rep
 
 				oldVote, _ := oldState.GetVote(chainName, proposalID, wallet)
 				newVote, _ := newState.GetVote(chainName, proposalID, wallet)
-
-				oldProposal, oldProposalFound := oldState.GetProposal(chainName, proposalID)
-				newProposal := proposalVotes.Proposal
-
-				if oldProposalFound {
-					// There can be the following cases:
-					// 1) old proposal not found - this is a new proposal, no custom logic needed.
-					// 2) old proposal is found, it's not in voting and the current one is not in voting
-					// (like deposit -> deposit) - we don't need to report it at all.
-					// 3) old proposal is found, it's not in voting but the current one is in voting
-					// (like deposit -> deposit) - no custom logic needed.
-					// 4) old proposal is found, it's in voting and the current one is in voting
-					// (like voting -> voting) - no custom logic needed.
-					// 5) old proposal is found, it's in voting and the current one is not in voting
-					// (like voting -> passed) -> send a new event that the voting has finished.
-
-					// case 1, 3 and 4 is handled outside of this if case
-					if oldProposal.IsInVoting() && !newProposal.IsInVoting() { // case 5
-						g.Logger.Debug().
-							Str("chain", chainName).
-							Str("proposal", proposalID).
-							Str("wallet", wallet).
-							Msg("Previously proposal was in voting, but it's not now - sending an alert")
-
-						entries = append(entries, events.FinishedVotingEvent{
-							Chain:    g.Chains.FindByName(chainName),
-							Proposal: newProposal,
-						})
-						continue
-					} else if !oldProposal.IsInVoting() && !newProposal.IsInVoting() { // case 2
-						g.Logger.Debug().
-							Str("chain", chainName).
-							Str("proposal", proposalID).
-							Str("wallet", wallet).
-							Msg("Previously proposal was and is not in voting period - ignoring.")
-						continue
-					}
-				}
 
 				// Error querying for vote - need to notify via Telegram.
 				if newVote.IsError() {
